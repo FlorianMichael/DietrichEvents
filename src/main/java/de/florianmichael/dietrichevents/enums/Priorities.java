a55f59da988d56fab919b9b57139e29e15555465
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-05-28 15:38:22 +0200

    Added Priorities class

---
+++ src/main/java/de/florianmichael/dietrichevents/enums/Priorities.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of DietrichEvents - https://github.com/FlorianMichael/DietrichEvents
+ * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.florianmichael.dietrichevents.enums;
+
+public class Priorities {
+    public final static int LOWEST = -2;
+    public final static int LOW = -1;
+    public final static int NONE = 0;
+    public final static int HIGH = 1;
+    public final static int HIGHEST = 2;
+
+    public final static int MONITOR = Integer.MAX_VALUE;
+    public final static int FIRST = Integer.MIN_VALUE;
+}
