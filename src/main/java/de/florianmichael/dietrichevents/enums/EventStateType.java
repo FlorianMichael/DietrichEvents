0606d65da0a128aa776d94ae259af0457ebcdd3f
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-01 17:07:15 +0100

    2024

--- src/main/java/de/florianmichael/dietrichevents/enums/EventStateType.java
+++ src/main/java/de/florianmichael/dietrichevents/enums/EventStateType.java
@@ -1,12 +1,12 @@
 /*
  * This file is part of DietrichEvents - https://github.com/FlorianMichael/DietrichEvents
- * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ * Copyright (C) 2023-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package de.florianmichael.dietrichevents.enums;
 
 /**
